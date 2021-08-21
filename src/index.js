79d7433301a6705803c47f3741cc5e60e9dae869
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2021-08-21 20:35:52 +0600

    Update index.js
    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const { interfaces } = require("mocha");
+
 module.exports = function reverse (n) {
-  
+  if(n < 0)
+  n = n*-1;
+  let str = String(n);
+  result =0;
+  for( let i = 0 ; i < str.length ; i++)
+  {
+    result += str[i] * (10**i);
+  }
+  return result;
 }
